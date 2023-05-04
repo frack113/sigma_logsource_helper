416c4b86a2da4b96610f2c85bea339f79f7a7e7b
frack113 <62423083+frack113@users.noreply.github.com>
2023-05-04 19:39:54 +0200

    Add Qt6 Quizz
    Signed-off-by: frack113 <62423083+frack113@users.noreply.github.com>

--- quizz.py
+++ quizz.py
@@ -7,8 +7,22 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi('mainwindow.ui', self)
 
-        self.button_load = self.findChild(QtWidgets.QPushButton, 'Button_load') # Find the button
-        self.button_load.clicked.connect(self.load) # Remember to pass the definition/method, not the return value!
+        self.list_q1 = self.findChild(QtWidgets.QListWidget, 'list_q1')
+        self.dictq1 ={}
+        self.button_v1 = self.findChild(QtWidgets.QPushButton, 'valide_1')
+        self.button_v1.clicked.connect(self.itemClicked_event_v1)
+
+        self.list_q2 = self.findChild(QtWidgets.QListWidget, 'list_q2')
+        self.dictq2 = {}
+        self.button_v2 = self.findChild(QtWidgets.QPushButton, 'valide_2')
+        self.button_v2.clicked.connect(self.itemClicked_event_v2)
+
+        self.list_q3 = self.findChild(QtWidgets.QListWidget, 'list_q3')
+        self.dictq3 = {}
+        self.button_v3 = self.findChild(QtWidgets.QPushButton, 'valide_3')
+        self.button_v3.clicked.connect(self.itemClicked_event_v3)
+       
+        self.table_log = self.findChild(QtWidgets.QTableWidget, 'tableWidget')
 
         self.yaml = YAML()
         self.yaml.preserve_quotes = True
@@ -18,17 +32,61 @@ class Ui(QtWidgets.QMainWindow):
         self.yaml.indent(mapping=4, sequence=4, offset=4)
         self.yaml_data = {}
 
+        self.load()
         self.show()
-
-
+    
     def load(self):
         with open('logsource.yml', encoding="UTF-8") as file_in:
             self.yaml_data = self.yaml.load(file_in)
 
-        list_q1 = self.findChild(QtWidgets.QListView, 'listView_1')
+        self.list_q1.clear()
+        i = 0 # dirty
         for uuid in self.yaml_data['question_general']:
-            QtWidgets.QListWidgetItem(str(self.yaml_data['question_general'][uuid]['ask']),list_q1)
+            self.list_q1.addItem(self.yaml_data['question_general'][uuid]['ask'])
+            self.dictq1[i]=uuid
+            i += 1
 
+    def itemClicked_event_v1(self):
+        self.list_q2.clear()
+        self.dictq2 = {}
+        i = 0
+        for item in self.list_q1.selectedItems():
+            row = self.list_q1.row(item)
+            for next_uuid in self.yaml_data['question_general'][self.dictq1[row]]['uuid_ref']:
+                self.dictq2[i] = next_uuid
+                i += 1
+                self.list_q2.addItem(self.yaml_data['questions_type'][next_uuid]['ask'])
+
+    def itemClicked_event_v2(self):
+        self.list_q3.clear()
+        self.dictq3 = {}
+        i = 0
+        for item in self.list_q2.selectedItems():
+            row = self.list_q2.row(item)
+            for next_uuid in self.yaml_data['questions_type'][self.dictq2[row]]['uuid_ref']:
+                self.dictq3[i] = next_uuid
+                i += 1
+                self.list_q3.addItem(self.yaml_data['questions_logsources'][next_uuid]['ask'])
+
+    def itemClicked_event_v3(self):
+        self.table_log.setRowCount(0) #clean ?
+        data =[('product','category','service')]
+        for item in self.list_q3.selectedItems():
+            q3_row = self.list_q3.row(item)
+            for next_uuid in self.yaml_data['questions_logsources'][self.dictq3[q3_row]]['uuid_ref']:
+                row_data=(self.yaml_data['logsources'][next_uuid]['product'],
+                    self.yaml_data['logsources'][next_uuid]['category'],
+                    self.yaml_data['logsources'][next_uuid]['service'])
+                data.append(row_data)
+        
+        numrows = len(data)
+        numcols = len(data[0])
+        self.table_log.setColumnCount(numcols)
+        self.table_log.setRowCount(numrows)
+
+        for row in range(numrows):
+            for column in range(numcols):
+                 self.table_log.setItem(row, column, QtWidgets.QTableWidgetItem((data[row][column])))
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
